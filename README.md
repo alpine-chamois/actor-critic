45aa80eadb91e58227d1960a28006b7cd6da9b23
alpine-chamois <tim.dudman@gmail.com>
2023-06-28 20:12:43 +0100

    Updated to use an SB3 agent.

--- README.md
+++ README.md
@@ -1,55 +1,19 @@
 ## Advantage Actor-Critic (A2C) Reinforcement Learning (RL) Agent
 
-This A2C RL agent is based on the Asynchronous A2C (A3C) agent in [Deep Reinforcement Learning in Action](https://www.manning.com/books/deep-reinforcement-learning-in-action), but with tuned hyperparameters, and without asynchronous processing.
+This [Stable Baselines A2C agent](https://stable-baselines3.readthedocs.io/en/master/modules/a2c.html) learns to play the [Cart Pole](https://gymnasium.farama.org/environments/classic_control/cart_pole/) game environment in [Gymnasium](https://gymnasium.farama.org/content/basic_usage/):
 
-A2C agents combine a Deep Q-network (DQN) like that used by [DeepMind](https://www.deepmind.com/publications/playing-atari-with-deep-reinforcement-learning) with a policy network like REINFORCE. They provide direct sampling of actions from a distribution (like a policy network) whilst also supporting rapid online learning (like a DQN, but without the need for experience replay or a target network). 
-
-The A2C agent learns to play the [Cart Pole](https://gymnasium.farama.org/environments/classic_control/cart_pole/) game environment in [Gymnasium](https://gymnasium.farama.org/content/basic_usage/):
-
-![Agent-environment loop](images/agent_environment_loop.png)
-
-_OpenAI Gym, OpenAI, 2022_
-
-The agent is a two-headed feed-forward neural network:
-
-![A2C model](images/actor_critic_model.png)
-
-_Deep Reinforcement Learning in Action, Manning, 2020_
-
-Here the agent is being trained to play Cart Pole.
+Here the agent is being trained to play Cart Pole with the same hyperparameters as the PyTorch implementation on [main](https://github.com/alpine-chamois/actor-critic/tree/main).
 
 ![Training metrics](images/training-metrics.png)
 
-And here the trained agent is playing the game unaided:
+_Generated after training using TensorBoard:_ ```tensorboard --logdir ./a2c_cartpole_tensorboard/```
+
+It learns to play with the same performance in the same number of steps:
 
 ![Evaluations](images/evaluation.png)
 
-### What about playing other games?
-
-The A2C agent can be used to play any game as long as the size of the observation space and action space are set in ```actor-critic-agent.py``` and ```main.py``` is updated to correctly handle rewards. There is a [branch of this repository](https://github.com/alpine-chamois/actor-critic/tree/lunar-lander) that shows how it can be successfully trained to play the [Lunar Lander](https://gymnasium.farama.org/environments/box2d/lunar_lander/) game in Gymnasium.
-
-### Why A2C and not A3C or PPO?
-
-Although A3C and PPO agents can perform better than A2C agents, they include additional complexity that makes the fundamentals of RL more difficult to understand when looking at the code. This A2C agent is designed to be a reference for how to implement a Deep RL (DRL) agent using [PyTorch](https://pytorch.org/). If you want a PPO agent, I recommend using [the implementation in Stable Baselines 3](https://stable-baselines3.readthedocs.io/en/master/modules/ppo.html). There is a [branch of this repository](https://github.com/alpine-chamois/actor-critic/tree/stable-baselines) that shows how to implement an equivalent A2C agent using Stable Baselines 3, and to convert this agent to a PPO agent, simply replace ```A2C``` with ```PPO``` everywhere in ```main.py```. You can also experiment with the asynchronous abilities of PPO agents by using a [vectorised environment](https://gymnasium.farama.org/api/vector/).
-
-### Useful [Machine Learning Mastery](https://machinelearningmastery.com/) links for setting hyperparameters
-* [Choosing the number of hidden layers and neurons in a neural network](https://machinelearningmastery.com/how-to-configure-the-number-of-layers-and-nodes-in-a-neural-network/)
-* [Choosing activation functions for a neural network](https://machinelearningmastery.com/choose-an-activation-function-for-deep-learning/)
-* [Choosing the learning rate for a neural network](https://machinelearningmastery.com/learning-rate-for-deep-learning-neural-networks/)
-
-### [Colin Skow](https://github.com/colinskow)'s excellent reinforcement learning tutorials
-
-* [Bellman Equation Basics for Reinforcement Learning](https://www.youtube.com/watch?v=14BfO5lMiuk)
-* [Bellman Equation Advanced for Reinforcement Learning](https://www.youtube.com/watch?v=aNuOLwojyfg)
-* [Dynamic Programming Tutorial for Reinforcement Learning](https://www.youtube.com/watch?v=aAkFtRxeP7c)
-* [Monte Carlo Reinforcement Learning Tutorial](https://www.youtube.com/watch?v=mMEFFN1H5Cg)
-* [Policy Gradient Methods Tutorial](https://www.youtube.com/watch?v=0c3r5EWeBvo)
-* [Actor Critic (A3C) Tutorial](https://www.youtube.com/watch?v=O5BlozCJBSE)
-* [Continuous Action Space Actor Critic Tutorial](https://www.youtube.com/watch?v=kWHSH2HgbNQ)
-* [Proximal Policy Optimization (PPO) Tutorial](https://www.youtube.com/watch?v=WxQfQW48A4A)
-
-### [Rudy Gilman](https://rudygilman.com/)'s story explaining A2C in terms of animal learning
-* [Intuitive RL: Intro to Advantage-Actor-Critic (A2C)](https://medium.com/hackernoon/intuitive-rl-intro-to-advantage-actor-critic-a2c-4ff545978752)
+### Stable Baselines links
+[Reinforcement Learning Tips and Tricks](https://stable-baselines3.readthedocs.io/en/master/guide/rl_tips.html).
 
 ### Getting started (Windows)
 
--- src/actorcritic/actor_critic_agent.py
+++ src/actorcritic/actor_critic_agent.py
@@ -1,20 +1,16 @@
 """
 ActorCriticAgent
 """
-import math
-import sys
-from typing import SupportsFloat
-
+import torch
 import gymnasium as gym
-import matplotlib.pyplot as pyplot
-import numpy
 # noinspection PyUnresolvedReferences
 import pygame  # Required for rendering
-import torch
-from gymnasium.core import ObsType
-from torch import nn, Tensor
+from stable_baselines3 import A2C
+import stable_baselines3.common.on_policy_algorithm as algorithm
+from stable_baselines3.common.callbacks import BaseCallback, EvalCallback, StopTrainingOnRewardThreshold
+from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.vec_env import VecEnv
 
-from actorcritic.actor_critic_model import ActorCriticModel
 from actorcritic.agent import Agent
 
 
@@ -35,13 +31,19 @@ class ActorCriticAgent(Agent):
         # Training and evaluation parameters
         self.game: str = game
         self.max_cumulative_reward: float = max_cumulative_reward
+        # Specify network parameters to match the PyTorch agent (output layers and activation functions are
+        # automatically set)
+        # Actor (4/64/128/2)
+        # Critic (4/64/128/64/1)
+        self.net_arch = dict(pi=[64, 128], vf=[64, 128, 64])
+        self.activation_fn = torch.nn.ReLU
         self.n_steps: int = 10  # Steps between optimisations
         self.learning_rate: float = 0.001
-        self.gamma: float = 0.95  # The Bellman discount factor
-        self.critic_loss_scaling: float = 0.1
         self.entropy_coefficient: float = 0.00
-        self.model_file: str = 'a2c.mdl'
+        self.gamma: float = 0.95  # The Bellman discount factor
         self.averaging_window: int = 10
+        self.eval_freq: int = 1000  # Steps between evaluations
+        self.model_file: str = 'a2c.mdl'
 
     def train(self) -> None:
         """
@@ -49,266 +51,55 @@ class ActorCriticAgent(Agent):
         """
         # Create environment and agent
         environment: gym.Env = gym.make(self.game)
-        observations: int = environment.observation_space.shape[0]
-        actions: int = environment.action_space.n
-        model: nn.Module = ActorCriticModel(observations=observations, actions=actions)
+        policy_kwargs = dict(activation_fn=self.activation_fn, net_arch=self.net_arch)
+        agent: algorithm.OnPolicyAlgorithm = A2C("MlpPolicy", environment, policy_kwargs=policy_kwargs,
+                                                 n_steps=self.n_steps, learning_rate=self.learning_rate,
+                                                 gamma=self.gamma, verbose=1, ent_coef=self.entropy_coefficient,
+                                                 tensorboard_log="./a2c_cartpole_tensorboard/")
 
-        # Create a gradient descent optimiser
-        optimizer: torch.optim.Optimizer = torch.optim.Adam(model.parameters(), lr=self.learning_rate)
+        # Train the agent
+        callback_on_best: BaseCallback = StopTrainingOnRewardThreshold(reward_threshold=self.max_cumulative_reward,
+                                                                       verbose=1)
+        eval_callback: BaseCallback = EvalCallback(Monitor(environment), callback_on_new_best=callback_on_best,
+                                                   eval_freq=self.eval_freq, n_eval_episodes=self.averaging_window)
+        # Set huge number of steps because termination is based on the callback
+        agent.learn(int(1e10), callback=eval_callback)
 
-        # Store episode metrics
-        episode: int = 0
-        cumulative_rewards: list[float] = []
-        average_cumulative_rewards: list[float] = []
-        actor_losses: list[float] = []
-        critic_losses: list[float] = []
-        entropy_losses: list[float] = []
-
-        while self.is_below_performance_threshold(average_cumulative_rewards):
-
-            # Reset the environment and observe initial state
-            observation, info = environment.reset()
-
-            # Initialise training variables
-            step: int = 0
-            terminated: bool = False
-            truncated: bool = False
-            cumulative_reward: float = 0
-            predicted_next_value: Tensor = torch.Tensor([0])
-
-            # Initialise rollout buffers
-            predicted_values: list[Tensor] = []
-            log_action_probabilities: list[Tensor] = []
-            rewards: list[SupportsFloat] = []
-            entropies: list[Tensor] = []
-
-            while not terminated and not truncated:
-
-                # Run the agent
-                policy, value = model(torch.from_numpy(observation).float())
-
-                # Determine the action
-                logits = policy.view(-1)
-                action_distribution = torch.distributions.Categorical(logits=logits)
-                action = action_distribution.sample()
-
-                # Store the value and action probability
-                predicted_values.append(value)
-                log_action_probability = policy.view(-1)[action]
-                log_action_probabilities.append(log_action_probability)
-
-                # Calculate and store the entropy
-                probability = torch.exp(log_action_probability)
-                entropy = -(log_action_probability * probability).sum()
-                entropies.append(entropy)
-
-                # Perform the action on the environment, receive a reward and observe the new state
-                observation, reward, terminated, truncated, info = environment.step(action.detach().numpy())
-
-                # Reward shaping
-                reward = self.shape_reward(observation, reward, terminated, truncated)
-
-                # Increment the step and cumulative reward
-                step += 1
-                cumulative_reward += reward
-
-                # Store the reward
-                rewards.append(reward)
-
-                # Optimise after N steps or at the end of the episode
-                if step == self.n_steps or terminated or truncated:
-
-                    # Record the predicted value of the next state if not terminated
-                    if not terminated:
-                        predicted_next_value = value.detach()  # Detach to stop double backpropagation
-
-                    # Optimise
-                    actor_loss, critic_loss, entropy_loss = self.optimise(model, optimizer, log_action_probabilities,
-                                                                          rewards,
-                                                                          predicted_values, predicted_next_value,
-                                                                          entropies)
-                    actor_losses.append(actor_loss)
-                    critic_losses.append(critic_loss)
-                    entropy_losses.append(entropy_loss)
-
-                    # Reset the rollout buffers and training variables
-                    predicted_values, log_action_probabilities, rewards, entropies = [], [], [], []
-                    predicted_next_value = torch.Tensor([0])
-                    step = 0
-
-            # Increment the episode
-            episode += 1
-
-            # Store the cumulative reward
-            cumulative_rewards.append(cumulative_reward)
-            average_cumulative_rewards = self.average_metrics(cumulative_rewards)
-
-            # Print training progress
-            self.print_training_progress(episode, average_cumulative_rewards)
-
-        # Plot training metrics
-        average_actor_losses: list[float] = self.average_metrics(actor_losses)
-        average_critic_losses: list[float] = self.average_metrics(critic_losses)
-        average_entropy_losses: list[float] = self.average_metrics(entropy_losses)
-        self.plot_training_metrics(average_cumulative_rewards, average_actor_losses, average_critic_losses,
-                                   average_entropy_losses)
+        # Note that rollout metrics are over 100 episodes, and eval metrics use deterministic actions
 
         # Save the agent
-        torch.save(model.state_dict(), self.model_file)
-
-    def optimise(self, agent: nn.Module, optimizer: torch.optim.Optimizer, log_action_probabilities: list[Tensor],
-                 rewards: list[SupportsFloat], values: list[Tensor], predicted_next_value: Tensor,
-                 entropies: list[Tensor]) -> tuple[float, float, float]:
-        """
-        Optimise
-        :param agent: the agent
-        :param optimizer: the optimiser
-        :param log_action_probabilities: the log action probabilities
-        :param rewards: the rewards
-        :param values: the values
-        :param predicted_next_value: the predicted value to bootstrap
-        :param entropies: the entropies
-        :return: the actor loss, critic_loss and entropy_loss
-        """
-        # Reverse the episode data so most recent data comes first in 1D tensors
-        rewards_: Tensor = torch.Tensor(rewards).flip(dims=(0,)).view(-1)
-        log_action_probabilities_: Tensor = torch.stack(log_action_probabilities).flip(dims=(0,)).view(-1)
-        values_: Tensor = torch.stack(values).flip(dims=(0,)).view(-1)
-        entropies_: Tensor = torch.stack(entropies).flip(dims=(0,)).view(-1)
-
-        # Apply the Bellman equation to calculate the value of the current state
-        returns: list[Tensor] = []
-        return_: Tensor = predicted_next_value  # Bootstrap the predicted value
-        for reward in range(rewards_.shape[0]):
-            return_ = rewards_[reward] + self.gamma * return_
-            returns.append(return_)
-        # Make tensor 1D
-        returns_: Tensor = torch.stack(returns).view(-1)
-        # Normalise returns between -1 and 1
-        returns_ = torch.nn.functional.normalize(returns_, dim=0)
-
-        # Calculate advantage and loss
-        advantage: Tensor = returns_ - values_.detach()  # Detach to stop double backpropagation
-        actor_loss: Tensor = (-1 * log_action_probabilities_ * advantage).sum()
-        critic_loss: Tensor = torch.pow(values_ - returns_, 2).sum()
-        entropy_loss: Tensor = entropies_.detach().sum()  # Detach to stop double backpropagation
-
-        # Scale down the critic loss because we want the actor to learn faster than the critic
-        # Subtract the entropy as we want to encourage exploration
-        loss: Tensor = actor_loss + (self.critic_loss_scaling * critic_loss) - (self.entropy_coefficient *
-                                                                                entropy_loss)
-
-        # Calculate gradients
-        agent.zero_grad()
-        optimizer.zero_grad()
-        loss.backward()
-
-        # Optimise
-        optimizer.step()
-
-        return actor_loss.detach().numpy(), critic_loss.detach().numpy(), entropy_loss.detach().numpy()
+        agent.save(self.model_file)
 
     def evaluate(self) -> None:
         """
         Evaluation loop
         """
+
         # Create environment and agent
-        environment: gym.Env = gym.make(self.game, render_mode='human')
-        observations: int = environment.observation_space.shape[0]
-        actions: int = environment.action_space.n
-        model: nn.Module = ActorCriticModel(observations, actions)
+        environment: gym.Env = gym.make(self.game, render_mode="rgb_array")
+        agent: algorithm.OnPolicyAlgorithm = A2C.load(self.model_file, environment)
+        vector_environment: VecEnv = agent.get_env()
 
-        # Load the trained agent parameters
-        model.load_state_dict(torch.load(self.model_file))
-
-        # Put the agent in evaluation mode
-        model.eval()
+        # Check hyperparameters
+        print(agent.policy)
+        print('(n_steps): ' + str(agent.n_steps))
+        print('(gamma): ' + str(agent.gamma))
+        print('(ent_coef): ' + str(agent.ent_coef))
 
         # Reset the environment and observe the initial state
-        observation, info = environment.reset()
+        observation = vector_environment.reset()
 
         # Play the game
-        truncated: bool = False
-        terminated: bool = False
-        while not terminated and not truncated:
+        done: bool = False
+        while not done:
             # Run the agent
-            policy, value = model(torch.from_numpy(observation).float())
-
-            # Make the tensor 1D
-            logits = policy.view(-1)
-            # Determine the best action
-            action = logits.detach().numpy().argmax()
+            action, _states = agent.predict(observation, deterministic=True)
 
             # Perform the action on the environment and observe the new state
-            observation, reward, terminated, truncated, info = environment.step(action)
+            observation, reward, done, info = vector_environment.step(action)
 
             # Render the step
-            environment.render()
+            vector_environment.render("human")
 
-    @staticmethod
-    def shape_reward(_observation: ObsType, reward: SupportsFloat, _terminated: bool, _truncated: bool) -> \
-            SupportsFloat:
-        """
-        Shape reward
-        :param _observation: the observation
-        :param reward: the reward
-        :param _terminated: if the episode terminated
-        :param _truncated: if the episode truncated
-        :return: shaped reward
-        """
-        return reward
-
-    def is_below_performance_threshold(self, average_cumulative_rewards: list[float]) -> bool:
-        """
-        Determine if the performance is below the threshold
-        :param average_cumulative_rewards: the average cumulative rewards
-        """
-        return not average_cumulative_rewards or average_cumulative_rewards[-1] < self.max_cumulative_reward
-
-    def print_training_progress(self, episode: int, average_cumulative_rewards: list[float]) -> None:
-        """
-        Print progress to terminal
-        :param episode: the episode
-        :param average_cumulative_rewards: the average cumulative rewards
-        """
-        sys.stdout.write('\rLearning to play ' + self.game + ', average cumulative reward: ' +
-                         str(math.ceil(average_cumulative_rewards[-1])) + '/' + str(self.max_cumulative_reward) +
-                         ' after ' + str(episode) + ' training episodes.')
-        sys.stdout.flush()
-
-    def plot_training_metrics(self, average_cumulative_rewards: list[float], average_actor_losses: list[float],
-                              average_critic_losses: list[float], average_entropy_losses: list[float]) -> None:
-        """
-        Plot training metrics
-        :param average_cumulative_rewards: the average cumulative rewards
-        :param average_actor_losses: the average actor losses
-        :param average_critic_losses: the average critic losses
-        :param average_entropy_losses: the average entropy losses
-        """
-        pyplot.suptitle('Training metrics')
-        pyplot.subplot(1, 2, 1)
-        pyplot.plot(average_cumulative_rewards, color='tab:cyan')
-        pyplot.fill_between(range(len(average_cumulative_rewards)), average_cumulative_rewards, color='tab:cyan',
-                            alpha=0.5)
-        pyplot.xlim(left=self.averaging_window)
-        pyplot.xlabel('Episode')
-        pyplot.ylabel('Average cumulative reward')
-        pyplot.subplot(1, 2, 2)
-        pyplot.plot(average_actor_losses, color='tab:cyan', alpha=1.0, label='Actor')
-        pyplot.plot(average_critic_losses, color='tab:cyan', alpha=0.6, label='Critic')
-        pyplot.plot(average_entropy_losses, color='tab:cyan', alpha=0.2, label='Entropy')
-        pyplot.xlim(left=self.averaging_window)
-        pyplot.xlabel('Optimisation step')
-        pyplot.ylabel('Average loss')
-        pyplot.legend(loc="upper right")
-        pyplot.show()
-
-    def average_metrics(self, metrics: list[float]) -> list[float]:
-        """
-        Average metrics for plotting
-        :param metrics: the metrics
-        :return: averaged metrics
-        """
-        # Note that this wil discard rewards before the averaging window is full so the plot Y-axis intercept needs
-        # setting
-        return list(numpy.convolve(metrics, numpy.ones(self.averaging_window) / self.averaging_window, mode='valid'))
+        # Close the environment
+        vector_environment.close()
