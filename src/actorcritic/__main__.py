369acbe5078d7424f511b612652a838d72a43a7f
alpine-chamois <tim.dudman@gmail.com>
2023-06-28 20:09:10 +0100

    Updated to play Lunar Lander

--- src/actorcritic/__main__.py
+++ src/actorcritic/__main__.py
@@ -2,28 +2,9 @@
 Main
 """
 import argparse
-from typing import SupportsFloat
-
-from gymnasium.core import ObsType
 
 from actorcritic.actor_critic_agent import ActorCriticAgent
 from actorcritic.agent import Agent
-
-
-class CartPoleActorCriticAgent(ActorCriticAgent):
-    """
-    Subclass the agent to shape rewards
-    """
-
-    @staticmethod
-    def shape_reward(_observation: ObsType, reward: SupportsFloat, terminated: bool,
-                     _truncated: bool) -> SupportsFloat:
-        """
-        Penalise the agent for losing the game
-        """
-        if terminated:
-            reward = -10
-        return reward
 
 
 if __name__ == '__main__':
@@ -35,7 +16,7 @@ if __name__ == '__main__':
     args: argparse.Namespace = parser.parse_args()
 
     # Construct agent
-    agent: Agent = CartPoleActorCriticAgent(game='CartPole-v1', max_cumulative_reward=500)
+    agent: Agent = ActorCriticAgent(game='LunarLander-v2', max_cumulative_reward=200)
 
     # Train and evaluate
     if args.train is True:
